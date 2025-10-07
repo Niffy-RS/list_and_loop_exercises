b386c63c9d7268de33d413e6c831ed165d9483e5
jennifer <jennifer_rs16@hotmail.com>
2025-10-07 20:28:59 +1000

    completed challenges

--- challenges.py
+++ challenges.py
@@ -9,7 +9,8 @@ def add_chocolate(shopping_list: list):
     Returns:
         - the same list, with the string "chocolate" added to the end
     """
-    pass
+    shopping_list.append("chocolate")
+    return shopping_list
 
 def lou_bega(lyrics_list: list):
     """This function accepts a list of strings and adds the words 
@@ -36,7 +37,16 @@ def lou_bega(lyrics_list: list):
             "A little bit of Rita's all I need"
         ]
     """
-    pass
+    # 1. Make a list to put our stuff in
+    new_lyric_list = []
+    # 2. Going over everything in our original list
+    for lyric in lyrics_list:
+        # 3. Do some stuff to it
+        new_lyric = f"A little bit of {lyric}"
+        # 4. Add the result of some stuff to our list in step 1
+        new_lyric_list.append(new_lyric)
+    # 5. Return our list with stuff done to it
+    return new_lyric_list
 
 def assemble_guest_list():
     """This function repeatedly prompts the user for the name of a dinner guest.
@@ -49,7 +59,19 @@ def assemble_guest_list():
     Returns:
         - a list of strings supplied by the user
     """
-    pass
+    # 1. Make a list for our names
+    names = []
+    # 2. Ask for the names
+    name = input("What's the guests name? ")
+    # 3. Until the user enters nothing
+    while name != "":
+        # 4. Add name to list
+        names.append(name)
+        # 5. Ask for a name again
+        name = input("What's the guests name? ")
+        # 6. Return the list of names
+    return names
+
 
 def is_prime(some_number: int): # A bit trickier!
     """This function tests to see if the input is a prime number.
@@ -65,7 +87,18 @@ def is_prime(some_number: int): # A bit trickier!
         - a boolean representing whether or not some_number is prime
     """
 
-    pass 
+    # 1. If our input is 1 or 0, return false
+    if some_number < 2:
+        return False
+    # 2. Get our devisors and iterate over them
+    for i in range(2, some_number):
+        # 3. If there's no remainder, we've found a divisor
+        if some_number % i == 0:
+            return False
+    # 4. If we get here without any other divisors - this is prime
+    return True
+
+
     
     # Hint: 
     #   int(1.5) == 1.0
